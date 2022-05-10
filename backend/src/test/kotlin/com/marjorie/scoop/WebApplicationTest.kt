16f36c158a4be19ff8209cf3ea57017b03f2c183
Elizabeth <elizabethinposti@gmail.com>
2022-05-10 19:28:17 +0300

    Add web app test

---
+++ backend/src/test/kotlin/com/marjorie/scoop/WebApplicationTest.kt
@@ -0,0 +1,44 @@
+package com.marjorie.scoop
+
+import org.hamcrest.Matchers.containsString
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Tests HTTP requests without starting a server.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class WebApplicationTest {
+    @Autowired
+    private val mockMvc: MockMvc? = null
+
+    @Test
+    @Throws(Exception::class)
+    fun `Home contains a greeting message`() {
+        mockMvc!!
+            .perform(
+                get("/")
+            )
+            .andDo(
+                print()
+            )
+            .andExpect(
+                status().isOk
+            )
+            .andExpect(
+                content().string(
+                    containsString("Hello!")
+                )
+            )
+    }
+}
