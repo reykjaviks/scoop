9946b50f65813fb3a643bd264cdb7cc1b31736cf
Elizabeth <elizabethinposti@gmail.com>
2022-05-28 16:22:55 +0300

    Use injected Authentication object to display logged in user's info

--- backend/src/main/kotlin/com/marjorie/scoop/home/HomeController.kt
+++ backend/src/main/kotlin/com/marjorie/scoop/home/HomeController.kt
@@ -1,5 +1,6 @@
 package com.marjorie.scoop.home
 
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +9,5 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/")
 class HomeController {
     @GetMapping
-    fun home(): List<String> = listOf("Hello!", "Hello again!")
-
+    fun home(auth: Authentication): String = "Auth info: $auth"
 }
