58660d6dd3d2ba4a635b0d6ade88e72e2010dd04
Elizabeth <elizabethinposti@gmail.com>
2022-06-09 19:51:22 +0300

    Use OncePerRequestFilter instead of Filter

--- backend/src/main/kotlin/com/marjorie/scoop/auth/AuthenticationLoggingFilter.kt
+++ backend/src/main/kotlin/com/marjorie/scoop/auth/AuthenticationLoggingFilter.kt
@@ -1,22 +1,28 @@
 package com.marjorie.scoop.auth
 
 import mu.KotlinLogging
+import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.Filter
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 private val logger = KotlinLogging.logger {}
 
 /**
  * Used to log request IDs after a successful authentication.
  */
-class AuthenticationLoggingFilter: Filter {
-    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+class AuthenticationLoggingFilter: OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain)
+    {
         val httpRequest: HttpServletRequest = request as HttpServletRequest
-        val requestId = httpRequest.getHeader("request-id")
-        logger.info("Successfully authenticated request with ID '$requestId'")
-        chain.doFilter(request, response)
+        val requestID = httpRequest.getHeader("request-id")
+        logger.info("Successfully authenticated request with ID '$requestID'")
+        filterChain.doFilter(request, response)
     }
 }
