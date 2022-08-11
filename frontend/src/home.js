b358cccd8a3a3d4057b81f00a3187517048aab15
Elizabeth <elizabethinposti@gmail.com>
2022-08-11 15:33:28 +0300

    Initialize frontend

---
+++ frontend/src/home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="Home">
+            <header>
+                <p>Scoop Home</p>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
