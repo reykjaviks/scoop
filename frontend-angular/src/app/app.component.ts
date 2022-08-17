5b1b2ddc8ed77fe3c857150fe11e56062825cec4
Elizabeth <elizabethinposti@gmail.com>
2022-08-17 17:16:08 +0300

    Add Bootstrap

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {VenueGetDTO} from "./common/data";
 import {VenueService} from "./venue/venue.service";
 import {HttpErrorResponse} from "@angular/common/http";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ import {HttpErrorResponse} from "@angular/common/http";
 export class AppComponent implements OnInit {
   public venues: VenueGetDTO[] | undefined;
 
-  constructor(private venueService: VenueService) {}
+  constructor(private venueService: VenueService, private modalService: NgbModal) {}
 
   ngOnInit() {
     this.getVenues();
@@ -26,5 +27,10 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     )
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal)
+
   }
 }
