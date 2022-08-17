013bdf808a8ca46b4c4c9427ffa8559c6bfa5fd6
Elizabeth <elizabethinposti@gmail.com>
2022-08-17 21:15:56 +0300

    Add CORS config

--- backend/src/main/kotlin/com/marjorie/scoop/config/WebAuthorizationConfig.kt
+++ backend/src/main/kotlin/com/marjorie/scoop/config/WebAuthorizationConfig.kt
@@ -89,4 +89,25 @@ class WebAuthorizationConfig(
     fun csrfTokenRepository(): CsrfTokenRepository {
         return CustomCsrfTokenRepository()
     }
+
+    @Bean
+    fun corsFilter(): CorsFilter? {
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.allowedOrigins = Arrays.asList("http://localhost:4200")
+        corsConfiguration.allowedHeaders = Arrays.asList(
+            "Origin", "Access-Control-Allow-Origin", "Content-Type",
+            "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+            "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        )
+        corsConfiguration.exposedHeaders = Arrays.asList(
+            "Origin", "Content-Type", "Accept", "Authorization",
+            "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"
+        )
+        corsConfiguration.allowedMethods = Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        val urlBasedCorsConfigurationSource = UrlBasedCorsConfigurationSource()
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
+        return CorsFilter(urlBasedCorsConfigurationSource)
+    }
+
 }
