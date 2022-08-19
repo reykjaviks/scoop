5cbad0f634e7b3aa871148a909eda08570100ea1
Elizabeth <elizabethinposti@gmail.com>
2022-08-19 21:21:56 +0300

    Refactor nav

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -6,26 +6,41 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { HomeComponent } from './home/home.component';
 import { VenueComponent } from './venue/venue.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { MainNavComponent } from './navigation/main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     VenueComponent,
-    NavigationComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MainNavComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-  ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      MatMenuModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatIconModule,
+      LayoutModule,
+      MatSidenavModule,
+      MatListModule,
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
