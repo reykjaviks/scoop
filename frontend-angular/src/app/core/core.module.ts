50c741ef1ffbd53e9569ddce456bf34001aa9001
Elizabeth <elizabethinposti@gmail.com>
2022-08-22 12:27:53 +0300

    Clean file

--- frontend-angular/src/app/core/core.module.ts
+++ frontend-angular/src/app/core/core.module.ts
@@ -5,18 +5,6 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { RouterModule } from '@angular/router';
 
-import {MatCardModule} from "@angular/material/card";
-import {MatOptionModule} from "@angular/material/core";
-import {MatSelectModule} from "@angular/material/select";
-import {MatInputModule} from "@angular/material/input";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import {MatTableModule} from "@angular/material/table";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatIconModule} from "@angular/material/icon";
-
 @NgModule({
   declarations: [
     HeaderComponent
@@ -26,7 +14,6 @@ import {MatIconModule} from "@angular/material/icon";
     MatToolbarModule,
     MatButtonModule,
     RouterModule
-    
   ],
   exports: [
     HeaderComponent
